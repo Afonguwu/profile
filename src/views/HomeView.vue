75ca777c5a86f93117e276809cc14487ced40243
Afong <t225310917@gmail.com>
2025-02-13 15:41:22 +0800

    update

---
+++ src/views/HomeView.vue
@@ -0,0 +1,70 @@
+<script setup></script>
+
+<template>
+  <div class="banner">
+    <div>
+      <h1 class="h1 heading-secondary">PORTFOLIO</h1>
+      <p class="h3 text-light">Dora Xie</p>
+      <p class="h3 text-light">Front-end Engineer</p>
+    </div>
+  </div>
+  <main>
+    <section role="introduction">
+      <div class="bg-light introduction">
+        <div class="img-block"><img src="https://fakeimg.pl/200x200/" alt="" /></div>
+        <div>
+          <h2 class="heading-dark h2-chinese mb-1">
+            關於我
+            <span class="mark-secondary d-block text">About me</span>
+          </h2>
+          <p class="text-primary text lh-lg">
+            我是 Dora，一個熱愛學習與探索新知的人，喜歡專注於細節並追求完美呈現。
+            近期，我的目標是做到精雕細琢但不鑽牛角尖。平時我喜歡玩 RPG
+            遊戲、做刺繡或畫插圖，這些興趣幫助我在閒暇時放鬆心情、激發靈感。未來，我希望能保持熱情與毅力，持續提升自己的能力與價值。
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="bg-dark">
+      <div>
+        <svg width="100%"></svg>
+      </div>
+      <div>
+        <div>
+          <h2>專案名稱</h2>
+          <p>2024/01~2024/06</p>
+        </div>
+        <div>
+          <div>
+            <div>
+              <h3>展示頁面</h3>
+              <p></p>
+            </div>
+            <div>
+              <h3>使用技術</h3>
+              <p></p>
+            </div>
+            <div>
+              <h3>專案技術</h3>
+              <p></p>
+            </div>
+          </div>
+          <div>
+            <figure>
+              <img src="" alt="" />
+              <figcaption>Desktop</figcaption>
+            </figure>
+            <figure>
+              <img src="" alt="" />
+              <figcaption>Pad</figcaption>
+            </figure>
+            <figure>
+              <img src="" alt="" />
+              <figcaption>Mobile</figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
