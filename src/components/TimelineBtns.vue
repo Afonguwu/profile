4b0a7de6c49cd92b56e0a914ecb70d25a0d66196
Afong <t225310917@gmail.com>
2025-02-15 15:39:05 +0800

    layout done

---
+++ src/components/TimelineBtns.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" role="buttons" width="100%" height="100">
+    <line x1="2" y1="50" x2="99%" y2="50" style="stroke: #a98f78; stroke-width: 2" />
+    <line x1="95%" y1="40" x2="99%" y2="50" style="stroke: #a98f78; stroke-width: 2" />
+    <line x1="95%" y1="60" x2="99%" y2="50" style="stroke: #a98f78; stroke-width: 2" />
+    <circle r="1" cx="2" cy="50" fill="#a98f78" />
+    <circle r="1" cx="99%" cy="50" fill="#a98f78" />
+    <circle r="1" cx="95%" cy="40" fill="#a98f78" />
+    <circle r="1" cx="95%" cy="60" fill="#a98f78" />
+    <text x="5" y="75" fill="#a1dddd" font-size="12">2024</text>
+    <text x="80%" y="75" fill="#a1dddd" font-size="12">2025</text>
+    <!-- btn -->
+    <circle r="10" cx="25%" cy="50" fill="#6b9696" stroke="#a98f78" stroke-width="3" />
+    <circle r="10" cx="40%" cy="50" fill="#6b9696" stroke="#a98f78" stroke-width="3" />
+    <circle r="10" cx="55%" cy="50" fill="#6b9696" stroke="#a98f78" stroke-width="3" />
+    <circle r="10" cx="70%" cy="50" fill="#a1dddd" stroke="#a98f78" stroke-width="3" />
+  </svg>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,22 @@
-<script setup></script>
+<script setup>
+import TimelineBtns from '@/components/TimelineBtns.vue'
+</script>
 
 <template>
   <div class="banner">
     <div>
-      <h1 class="h1 heading-secondary">PORTFOLIO</h1>
+      <h1 class="h1 heading-brown-200">PORTFOLIO</h1>
       <p class="h3 text-light">Dora Xie</p>
       <p class="h3 text-light">Front-end Engineer</p>
     </div>
   </div>
   <main>
-    <section role="introduction">
-      <div class="bg-light introduction">
+    <section class="bg-light" role="introduction">
+      <div class="introduction">
         <div class="img-block"><img src="https://fakeimg.pl/200x200/" alt="" /></div>
         <div>
-          <h2 class="heading-dark h2-chinese mb-1">
-            關於我
-            <span class="mark-secondary d-block text">About me</span>
-          </h2>
-          <p class="text-primary text lh-lg">
+          <h2 class="heading-dark h2-chinese mb-1">About</h2>
+          <p class="text-blue-400 text lh-lg">
             我是 Dora，一個熱愛學習與探索新知的人，喜歡專注於細節並追求完美呈現。
             近期，我的目標是做到精雕細琢但不鑽牛角尖。平時我喜歡玩 RPG
             遊戲、做刺繡或畫插圖，這些興趣幫助我在閒暇時放鬆心情、激發靈感。未來，我希望能保持熱情與毅力，持續提升自己的能力與價值。
@@ -25,46 +24,73 @@
         </div>
       </div>
     </section>
-    <section class="bg-dark">
+    <section class="bg-dark work">
+      <h2 class="heading-brown-200 h2-chinese mb-1">Works</h2>
       <div>
-        <svg width="100%"></svg>
+        <TimelineBtns />
       </div>
       <div>
-        <div>
-          <h2>專案名稱</h2>
-          <p>2024/01~2024/06</p>
+        <div class="bg-orange w-fit p-1 mb-1">
+          <h2 class="heading-dark h3">專案名稱</h2>
+          <p class="text-blue-300 fw-b">2024/01~2024/06</p>
         </div>
         <div>
-          <div>
-            <div>
-              <h3>展示頁面</h3>
-              <p></p>
-            </div>
-            <div>
-              <h3>使用技術</h3>
-              <p></p>
-            </div>
-            <div>
-              <h3>專案技術</h3>
-              <p></p>
-            </div>
+          <div class="img-block">
+            <figure>
+              <img src="https://fakeimg.pl/100x80/" alt="" />
+              <figcaption class="text-center">Desktop</figcaption>
+            </figure>
+            <figure>
+              <img src="https://fakeimg.pl/100x80/" alt="" />
+              <figcaption class="text-center">Pad</figcaption>
+            </figure>
+            <figure>
+              <img src="https://fakeimg.pl/100x80/" alt="" />
+              <figcaption class="text-center">Mobile</figcaption>
+            </figure>
           </div>
           <div>
-            <figure>
-              <img src="" alt="" />
-              <figcaption>Desktop</figcaption>
-            </figure>
-            <figure>
-              <img src="" alt="" />
-              <figcaption>Pad</figcaption>
-            </figure>
-            <figure>
-              <img src="" alt="" />
-              <figcaption>Mobile</figcaption>
-            </figure>
+            <div class="mb-1">
+              <h3 class="text-blue-400 fw-b">展示頁面</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facere alias cumque
+                maiores aperiam. Dignissimos corrupti sit illum repellat nesciunt? Quod quae non,
+                libero unde possimus accusamus. Doloribus, quia necessitatibus.
+              </p>
+            </div>
+            <div class="mb-1">
+              <h3 class="text-blue-400 fw-b">使用技術</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo non necessitatibus
+                dolorum, eveniet ratione beatae iure asperiores nemo et voluptates facere
+                repellendus ducimus cupiditate laborum, ea architecto? Ullam, modi porro.
+              </p>
+            </div>
+            <div class="mb-1">
+              <h3 class="text-blue-400 fw-b">專案技術</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error corrupti
+                exercitationem maiores labore molestiae quasi. Nisi, ad! Et delectus voluptatem
+                omnis, recusandae dolores, quod deleniti dolorum in, nemo repellat consequatur!
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </section>
   </main>
+  <section class="bg-light footer" role="footer">
+    <div>
+      <img src="https://fakeimg.pl/50x50/" alt="GitHub" />
+      <p class="text-blue-200 fw-b">GitHub</p>
+    </div>
+    <div>
+      <img src="https://fakeimg.pl/50x50/" alt="Cake" />
+      <p class="text-blue-300 fw-b">Cake</p>
+    </div>
+    <div>
+      <img src="https://fakeimg.pl/50x50/" alt="Gmail" />
+      <p class="text-blue-400 fw-b">Gmail</p>
+    </div>
+  </section>
 </template>
